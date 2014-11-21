2c8841d6da136b1775ce9ef6be645ce2990ef0b8
Guillaume Viguier <guillaume@viguierjust.com>
2014-11-21 12:31:19 +0100

    Exposing global cluster fields

--- plugins/restful/node/hr_sector/1.0/RestfulEntityTaxonomyTermGlobalClusters.class.php
+++ plugins/restful/node/hr_sector/1.0/RestfulEntityTaxonomyTermGlobalClusters.class.php
@@ -13,6 +13,19 @@ class RestfulEntityTaxonomyTermGlobalClusters extends \RestfulEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['acronym'] = array(
+      'property' => 'field_acronym',
+    );
+
+    $public_fields['homepage'] = array(
+      'property' => 'field_website',
+      'sub_property' => 'url',
+    );
+
+    $public_fields['icon'] = array(
+      'property' => 'field_image',
+    );
+
     return $public_fields;
   }
 
