5d07ae437ce9a4fa82a3ee312962711ebdda39b3
Guillaume Viguier <guillaume@viguierjust.com>
2014-11-21 09:41:46 +0100

    Exposing organization fields

--- plugins/restful/taxonomy_term/hr_organization/1.0/RestfulEntityTaxonomyTermOrganizations.class.php
+++ plugins/restful/taxonomy_term/hr_organization/1.0/RestfulEntityTaxonomyTermOrganizations.class.php
@@ -7,4 +7,23 @@
 
 class RestfulEntityTaxonomyTermOrganizations extends \RestfulEntityBaseTaxonomyTerm {
 
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['acronym'] = array(
+      'property' => 'field_acronym',
+      'sub_property' => 'value',
+    );
+
+    $public_fields['homepage'] = array(
+      'property' => 'field_website',
+      'sub_property' => 'url',
+    );
+
+    return $public_fields;
+  }
+
 }
