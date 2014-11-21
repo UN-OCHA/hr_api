0bfcb665a91d756023b05eb9625e656749154cc2
Guillaume Viguier <guillaume@viguierjust.com>
2014-11-21 12:25:09 +0100

    Expose global clusters

---
+++ plugins/restful/node/hr_sector/1.0/RestfulEntityTaxonomyTermGlobalClusters.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \RestfulEntityTaxonomyTermGlobalClusters.
+ */
+
+class RestfulEntityTaxonomyTermGlobalClusters extends \RestfulEntityBaseNode {
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    return $public_fields;
+  }
+
+}
