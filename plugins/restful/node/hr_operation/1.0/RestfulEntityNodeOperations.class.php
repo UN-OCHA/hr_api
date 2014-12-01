28b5cf576dd183c129fe73d8333d2d07ec76c299
Guillaume Viguier <guillaume@viguierjust.com>
2014-12-01 16:00:32 +0100

    Exposing operations

---
+++ plugins/restful/node/hr_operation/1.0/RestfulEntityNodeOperations.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \RestfulEntityNodeOperations.
+ */
+
+class RestfulEntityNodeOperations extends \RestfulEntityBaseNode {
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    return $public_fields;
+  }
+
+}
