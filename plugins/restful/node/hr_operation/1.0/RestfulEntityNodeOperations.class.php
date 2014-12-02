11bcdb627dff9b54fcd5a23a990be5cd432f52d7
Guillaume Viguier <guillaume@viguierjust.com>
2014-12-02 16:17:17 +0100

    Added fields for operations

--- plugins/restful/node/hr_operation/1.0/RestfulEntityNodeOperations.class.php
+++ plugins/restful/node/hr_operation/1.0/RestfulEntityNodeOperations.class.php
@@ -26,6 +26,22 @@ class RestfulEntityNodeOperations extends \RestfulEntityBaseNode {
       'property' => 'field_operation_type',
     );
 
+    $public_fields['status'] = array(
+      'property' => 'field_operation_status',
+    );
+
+    $public_fields['country'] = array(
+      'property' => 'field_country',
+      'resource' => array(
+        'hr_location' => 'locations',
+      ),
+    );
+
+    $public_fields['launch_date'] = array(
+      'property' => 'field_launch_date',
+      'sub_property' => 'value',
+    );
+
     return $public_fields;
   }
 
