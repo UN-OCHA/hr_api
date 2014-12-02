80f0f25bceaef1694993d0e9bb20eda830eb2cc1
Guillaume Viguier <guillaume@viguierjust.com>
2014-12-02 16:11:57 +0100

    Added fields for operations

--- plugins/restful/node/hr_operation/1.0/RestfulEntityNodeOperations.class.php
+++ plugins/restful/node/hr_operation/1.0/RestfulEntityNodeOperations.class.php
@@ -13,6 +13,19 @@ class RestfulEntityNodeOperations extends \RestfulEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['homepage'] = array(
+      'property' => 'field_website',
+      'sub_property' => 'url',
+    );
+
+    $public_fields['email'] = array(
+      'property' => 'field_email',
+    );
+
+    $public_fields['type'] = array(
+      'property' => 'field_operation_type',
+    );
+
     return $public_fields;
   }
 
